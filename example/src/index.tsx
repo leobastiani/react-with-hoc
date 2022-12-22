7bc065c8ab2b69f2acdfcaa67714a1915b504161
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2022-12-22 12:58:11 -0300

    Added example

---
+++ example/src/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { componentDisplayName } from "react-new-hoc";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+function Example() {
+  return <div />;
+}
+
+console.log(
+  "componentDisplayName.get(Example):",
+  componentDisplayName.get(Example)
+);
+
+root.render(
+  <React.StrictMode>
+    <div>
+      <h2>Default counter</h2>
+    </div>
+    <hr />
+    <div>
+      <h2>Counter with predefined value</h2>
+    </div>
+  </React.StrictMode>
+);
