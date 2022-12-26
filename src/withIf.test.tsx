7b5cee2c70782f56a0472112a84cc2a373b49ee4
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2022-12-26 16:21:51 -0300

    Added withIf

---
+++ src/withIf.test.tsx
@@ -0,0 +1,62 @@
+import { render } from "@testing-library/react";
+import { componentDisplayName } from "./componentDisplayName";
+import { withIf } from "./withIf";
+
+interface ExampleProps {
+  a: number;
+}
+
+function Example(props: ExampleProps): JSX.Element {
+  return <pre>{JSON.stringify(props)}</pre>;
+}
+
+it("withIf name with propName", () => {
+  const Component = withIf("b")(Example);
+  expect(componentDisplayName.get(Component)).toBe("withIf.b(Example)");
+});
+
+it("withIf name with function", () => {
+  const Component = withIf(({ a }: ExampleProps) => a > 0, {
+    dependencyNames: ["a"],
+  })(Example);
+  expect(componentDisplayName.get(Component)).toBe(
+    `withIf.(["a"]) => boolean(Example)`
+  );
+});
+
+it("withIf with propName", () => {
+  const Component = withIf("enabled")(Example);
+  const { rerender } = render(<Component a={1} enabled={false} />);
+  expect(document.body.children[0].innerHTML).toBe("");
+  rerender(<Component a={1} enabled={true} />);
+  expect(document.body.children[0].innerHTML).toBe(
+    '<pre>{"a":1,"enabled":true}</pre>'
+  );
+});
+
+it("withIf with function", () => {
+  const Component = withIf(({ a }: ExampleProps) => a > 0, {
+    dependencyNames: ["a"],
+  })(Example);
+  const { rerender } = render(<Component a={0} />);
+  expect(document.body.children[0].innerHTML).toBe("");
+  rerender(<Component a={-1} />);
+  expect(document.body.children[0].innerHTML).toBe("");
+  rerender(<Component a={1} />);
+  expect(document.body.children[0].innerHTML).toBe('<pre>{"a":1}</pre>');
+});
+
+it("withIf with Then and Else", () => {
+  const Component = withIf("condition", {
+    Then: (props: ExampleProps) => <pre id="then">{JSON.stringify(props)}</pre>,
+    Else: (props: ExampleProps) => <pre id="else">{JSON.stringify(props)}</pre>,
+  })(Example);
+  const { rerender } = render(<Component a={0} condition={false} />);
+  expect(document.body.children[0].innerHTML).toBe(
+    '<pre id="else">{"a":0,"condition":false}</pre>'
+  );
+  rerender(<Component a={1} condition={true} />);
+  expect(document.body.children[0].innerHTML).toBe(
+    '<pre id="then">{"a":1,"condition":true}</pre>'
+  );
+});
---
+++ src/withIf.tsx
@@ -0,0 +1,82 @@
+import { ComponentType, FunctionComponent, ReactNode, useMemo } from "react";
+import { newHoc } from "./newHoc";
+import { render } from "./render";
+
+type ThenElse<Props extends {}> = {
+  Then: (props: Props) => ReactNode;
+  Else: (props: Props) => ReactNode;
+};
+
+interface WithIfHoc {
+  <Props extends {}, PropName extends string>(
+    propName: PropName,
+    options?: Partial<ThenElse<Props>>
+  ): <ClosureProps extends Props>(
+    Component: ComponentType<ClosureProps>
+  ) => FunctionComponent<ClosureProps & { [K in PropName]: boolean }>;
+
+  <Props extends {}>(
+    func: (props: Props) => boolean,
+    options: Partial<ThenElse<Props>> & { dependencyNames: string[] }
+  ): <ClosureProps extends Props>(
+    Component: ComponentType<ClosureProps>
+  ) => FunctionComponent<ClosureProps>;
+}
+
+export const withIf = ((): WithIfHoc => {
+  function withIf(
+    Component: ComponentType,
+    propNameOrFactory: string | Function,
+    {
+      dependencyNames,
+      Then,
+      Else,
+    }: {
+      dependencyNames?: string[];
+      Then?: Function;
+      Else?: Function;
+    } = {}
+  ): FunctionComponent {
+    if (process.env.NODE_ENV !== "production") {
+      console.assert(
+        typeof propNameOrFactory !== "function" || dependencyNames,
+        "withIf with function should have dependecyNames assigned"
+      );
+    }
+
+    Then ??= (props: any): JSX.Element => render(Component, props);
+    // eslint-disable-next-line react/display-name
+    Else ??= (): JSX.Element => <></>;
+
+    function WithIf(props: any): JSX.Element {
+      let condition: boolean;
+      if (typeof propNameOrFactory === "string") {
+        condition = props[propNameOrFactory];
+      } else {
+        // is function
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        condition = useMemo(
+          () => propNameOrFactory(props),
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+          dependencyNames!.map((key) => props[key])
+        );
+      }
+
+      if (condition) {
+        return Then!(props);
+      } else {
+        return Else!(props);
+      }
+    }
+    return WithIf;
+  }
+
+  return newHoc(withIf, {
+    dot(_Component, propNameOrFactory, { dependencyNames } = {}) {
+      if (typeof propNameOrFactory === "string") {
+        return propNameOrFactory;
+      }
+      return `(${JSON.stringify(dependencyNames)}) => boolean`;
+    },
+  }) as WithIfHoc;
+})();
