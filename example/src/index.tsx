2e2ac4e382c528e700caf453b4d69da0e0a0ffa4
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2022-12-27 23:59:37 -0300

    Better examples

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { withProp } from "react-new-hoc";
 import { Clock } from "./Clock";
+import { HourPointer } from "./Pointers";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
+const RedHour = withProp("color", "red")(HourPointer);
+
 root.render(
   <React.StrictMode>
     <Clock />
+    <Clock MinuteMarks={null} />
+    <Clock HourPointer={(): typeof RedHour => RedHour} />
   </React.StrictMode>
 );
