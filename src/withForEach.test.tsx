aef135d617fa620a63851752265e0a6f6cbee862
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-06-15 22:38:46 -0300

    withForEach

--- src/withForEach.test.tsx
+++ src/withForEach.test.tsx
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/react";
 import { componentDisplayName } from "./componentDisplayName";
-import { withMap } from "./withMap";
+import { withForEach } from "./withForEach";
 
 interface ExampleProps {
   a: number;
@@ -12,37 +12,37 @@ function Example(props: ExampleProps): JSX.Element {
   return <pre>{JSON.stringify(props)}</pre>;
 }
 
-it("withMap name", () => {
-  const Component = withMap(10)(Example);
-  expect(componentDisplayName.get(Component)).toBe("withMap.10(Example)");
+it("withForEach name", () => {
+  const Component = withForEach(10)(Example);
+  expect(componentDisplayName.get(Component)).toBe("withForEach.10(Example)");
 });
 
-it("withMap number", () => {
-  const Component = withMap(2)(Example);
+it("withForEach number", () => {
+  const Component = withForEach(2)(Example);
   render(<Component a={1} b={2} />);
   expect(document.body.children[0].innerHTML).toBe(
     '<pre>{"a":1,"b":2,"i":0}</pre><pre>{"a":1,"b":2,"i":1}</pre>'
   );
 });
 
-it("withMap array of string", () => {
-  const Component = withMap(["c", "d"])(Example);
+it("withForEach array of string", () => {
+  const Component = withForEach(["c", "d"])(Example);
   render(<Component a={1} b={2} />);
   expect(document.body.children[0].innerHTML).toBe(
     '<pre>{"a":1,"b":2,"i":0,"children":"c"}</pre><pre>{"a":1,"b":2,"i":1,"children":"d"}</pre>'
   );
 });
 
-it("withMap array of objects", () => {
-  const Component = withMap([{ c: 3 }, { d: 4 }])(Example);
+it("withForEach array of objects", () => {
+  const Component = withForEach([{ c: 3 }, { d: 4 }])(Example);
   render(<Component a={1} b={2} />);
   expect(document.body.children[0].innerHTML).toBe(
     '<pre>{"children":{"c":3},"c":3,"a":1,"b":2,"i":0}</pre><pre>{"children":{"d":4},"d":4,"a":1,"b":2,"i":1}</pre>'
   );
 });
 
-it("withMap object", () => {
-  const Component = withMap({ c: 3 })(Example);
+it("withForEach object", () => {
+  const Component = withForEach({ c: 3 })(Example);
   render(<Component a={1} b={2} />);
   expect(document.body.children[0].innerHTML).toBe(
     '<pre>{"children":3,"a":1,"b":2,"i":"c"}</pre>'
---
+++ src/withForEach.tsx
@@ -0,0 +1,68 @@
+import { ComponentType, FunctionComponent, Key } from "react";
+import { PartialBy } from "./@types/PartialBy";
+import { Hoc } from "./Hoc";
+import { newHoc } from "./newHoc";
+
+interface Options<IndexName extends string, ValueName extends string> {
+  indexName?: IndexName;
+  valueName?: ValueName;
+  key: (props: object) => Key;
+}
+
+interface WithForEachHoc {
+  <IndexName extends string = "i", ValueName extends string = "children">(
+    target: number | Array<unknown> | object,
+    options?: Options<IndexName, ValueName>
+  ): Hoc<PartialBy<IndexName | ValueName>>;
+}
+
+export const withForEach = newHoc(function withForEach(
+  Component: ComponentType,
+  target: number | Array<unknown> | object | string,
+  {
+    indexName = "i",
+    key = (props: any): Key => props[indexName],
+    valueName = "children",
+  } = {}
+): FunctionComponent {
+  return function WithForEach(props: any): JSX.Element {
+    let ret;
+    if (typeof target === "number") {
+      ret = Array.from({ length: target }).map((_v, i) => (
+        <Component
+          key={key({ ...props, [indexName]: i })}
+          {...{ ...props, [indexName]: i }}
+        />
+      ));
+    } else if (Array.isArray(target)) {
+      if (target.length > 0 && typeof target[0] != "object") {
+        ret = target.map((v, i) => (
+          <Component
+            key={key({ ...props, [indexName]: i, [valueName]: v })}
+            {...{ ...props, [indexName]: i, [valueName]: v }}
+          />
+        ));
+      } else {
+        ret = target.map((v, i) => (
+          <Component
+            key={key({ ...props, [indexName]: i })}
+            {...{ [valueName]: v, ...v, ...props, [indexName]: i }}
+          />
+        ));
+      }
+    } else if (typeof target === "object" && target !== null) {
+      ret = Object.entries(target).map(([index, value]) => (
+        <Component
+          key={key({ ...props, [indexName]: index })}
+          {...{ [valueName]: value, ...props, [indexName]: index }}
+        />
+      ));
+    } else if (process.env.NODE_ENV !== "production") {
+      throw new Error(
+        "withForEach: should be used with number, Array or object"
+      );
+    }
+
+    return <>{ret}</>;
+  };
+}) as WithForEachHoc;
