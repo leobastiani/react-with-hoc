3b3debdd947e33d7d6ba8ee481b45b16dffc6f6f
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-06-17 11:40:40 -0300

    withWrapper

---
+++ src/withWrapper.test.tsx
@@ -0,0 +1,64 @@
+import { render } from "@testing-library/react";
+import { componentDisplayName } from "./componentDisplayName";
+import { withWrapper } from "./withWrapper";
+
+interface ExampleProps {
+  a: number;
+  b: number;
+}
+
+function Example(props: ExampleProps): JSX.Element {
+  return <pre>{JSON.stringify(props)}</pre>;
+}
+
+function Wrapper(props: { children: JSX.Element }): JSX.Element {
+  const { children, ...rest } = props;
+  return <pre data-props={JSON.stringify(rest)}>{children}</pre>;
+}
+
+it("withWrapper name", () => {
+  const Component = withWrapper(Wrapper)(Example);
+  expect(componentDisplayName.get(Component)).toBe(
+    "withWrapper.Wrapper(Example)"
+  );
+});
+
+it("withWrapper with default params", () => {
+  const Component = withWrapper(Wrapper)(Example);
+  render(<Component a={0} b={1} />);
+  expect(document.body.children[0].innerHTML).toBe(
+    '<pre data-props="{}"><pre>{"a":0,"b":1}</pre></pre>'
+  );
+});
+
+it("withWrapper with empty pickProps", () => {
+  const Component = withWrapper(Wrapper, { pickProps: [] })(Example);
+  render(<Component a={0} b={1} />);
+  expect(document.body.children[0].innerHTML).toBe(
+    '<pre data-props="{}"><pre>{"a":0,"b":1}</pre></pre>'
+  );
+});
+
+it("withWrapper with empty omitProps", () => {
+  const Component = withWrapper(Wrapper, { omitProps: [] })(Example);
+  render(<Component a={0} b={1} />);
+  expect(document.body.children[0].innerHTML).toBe(
+    '<pre data-props="{&quot;a&quot;:0,&quot;b&quot;:1}"><pre>{"a":0,"b":1}</pre></pre>'
+  );
+});
+
+it("withWrapper with pickProps", () => {
+  const Component = withWrapper(Wrapper, { pickProps: ["a"] })(Example);
+  render(<Component a={0} b={1} />);
+  expect(document.body.children[0].innerHTML).toBe(
+    '<pre data-props="{&quot;a&quot;:0}"><pre>{"a":0,"b":1}</pre></pre>'
+  );
+});
+
+it("withWrapper with omitProps", () => {
+  const Component = withWrapper(Wrapper, { omitProps: ["a"] })(Example);
+  render(<Component a={0} b={1} />);
+  expect(document.body.children[0].innerHTML).toBe(
+    '<pre data-props="{&quot;b&quot;:1}"><pre>{"a":0,"b":1}</pre></pre>'
+  );
+});
---
+++ src/withWrapper.tsx
@@ -0,0 +1,80 @@
+import { Identity } from "hotscript";
+import { ComponentType, FunctionComponent } from "react";
+import { Hoc } from "./Hoc";
+import { newHoc } from "./newHoc";
+
+interface WithWrapperHoc {
+  (
+    Wrapper: ComponentType<any>,
+    options?:
+      | {
+          pickProps: string[];
+        }
+      | {
+          omitProps: string[];
+        }
+  ): Hoc<Identity>;
+}
+
+export const withWrapper = newHoc(function withWrapper(
+  Component: ComponentType<any>,
+  Wrapper: ComponentType<any>,
+  {
+    pickProps,
+    omitProps,
+  }: {
+    pickProps?: string[];
+    omitProps?: string[];
+  } = { pickProps: [] }
+): FunctionComponent {
+  if (process.env.NODE_ENV !== "production") {
+    const bothAssigned = pickProps && omitProps;
+    const noneAssigned = !pickProps && !omitProps;
+    if (bothAssigned || noneAssigned) {
+      throw new Error(
+        "withWrapper should have either pickProps or omitProps assigned"
+      );
+    }
+  }
+
+  const set = new Set(pickProps || omitProps);
+
+  return function WithWrapper(props: any): JSX.Element {
+    if (set.size === 0 && pickProps) {
+      return (
+        <Wrapper>
+          <Component {...props} />
+        </Wrapper>
+      );
+    }
+
+    if (set.size === 0 && omitProps) {
+      return (
+        <Wrapper {...props}>
+          <Component {...props} />
+        </Wrapper>
+      );
+    }
+
+    const parentProps = {} as any;
+    if (pickProps) {
+      for (const prop in props) {
+        if (set.has(prop)) {
+          parentProps[prop] = props[prop];
+        }
+      }
+    } else if (omitProps) {
+      for (const prop in props) {
+        if (!set.has(prop)) {
+          parentProps[prop] = props[prop];
+        }
+      }
+    }
+
+    return (
+      <Wrapper {...parentProps}>
+        <Component {...props} />
+      </Wrapper>
+    );
+  };
+}) as WithWrapperHoc;
