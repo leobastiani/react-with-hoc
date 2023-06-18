8a6f75f37a99ff71b21a32c5925f0be5d97a4e67
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-06-18 10:11:40 -0300

    test-d/withRename.test-d.tsx

--- src/withRename.tsx
+++ src/withRename.tsx
@@ -1,13 +1,20 @@
 import { Fn } from "hotscript";
 import { ComponentType, FunctionComponent } from "react";
+import { OptionalKeysOf, SetOptional } from "type-fest";
 import { Hoc } from "./Hoc";
 import { newHoc } from "./newHoc";
 
 interface WithRenameFn<NewProp extends string, OldProp extends string>
   extends Fn {
-  return: {
-    [K in NewProp]: this["arg0"][OldProp];
-  } & Omit<this["arg0"], OldProp>;
+  return: OldProp extends keyof this["arg0"]
+    ? SetOptional<
+        {
+          [K in NewProp]: this["arg0"][OldProp];
+        },
+        OldProp extends OptionalKeysOf<this["arg0"]> ? NewProp : never
+      > &
+        Omit<this["arg0"], OldProp>
+    : this["arg0"];
 }
 
 interface WithRenameHoc {
---
+++ test-d/withRename.test-d.tsx
@@ -0,0 +1,63 @@
+import { FunctionComponent } from "react";
+import { expectType } from "tsd";
+import { withRename } from "../src/withRename";
+
+{
+  // with required prop
+
+  const BeforeHoc: React.FC<{
+    ignored: string;
+    ignoredOptional?: number;
+    included: boolean;
+    includedOptional?: symbol;
+  }> = undefined as any;
+  const AfterHoc = withRename("newProp", "included")(BeforeHoc);
+  expectType<
+    FunctionComponent<{
+      newProp: boolean;
+      ignored: string;
+      ignoredOptional?: number;
+      includedOptional?: symbol;
+    }>
+  >(AfterHoc);
+}
+
+{
+  // with optional prop
+
+  const BeforeHoc: React.FC<{
+    ignored: string;
+    ignoredOptional?: number;
+    included: boolean;
+    includedOptional?: symbol;
+  }> = undefined as any;
+  const AfterHoc = withRename("newProp", "includedOptional")(BeforeHoc);
+  expectType<
+    FunctionComponent<{
+      newProp?: symbol;
+      included: boolean;
+      ignored: string;
+      ignoredOptional?: number;
+    }>
+  >(AfterHoc);
+}
+
+{
+  // with unknown prop
+
+  const BeforeHoc: React.FC<{
+    ignored: string;
+    ignoredOptional?: number;
+    included: boolean;
+    includedOptional?: symbol;
+  }> = undefined as any;
+  const AfterHoc = withRename("newProp", "unknown")(BeforeHoc);
+  expectType<
+    FunctionComponent<{
+      included: boolean;
+      ignored: string;
+      ignoredOptional?: number | undefined;
+      includedOptional?: symbol | undefined;
+    }>
+  >(AfterHoc);
+}
