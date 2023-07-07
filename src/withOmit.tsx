96106270dd382b271dea73285898305b9dd65ebb
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-07-07 18:01:07 -0300

    withOmit and withPick

--- src/withOmit.tsx
+++ src/withOmit.tsx
@@ -1,16 +1,11 @@
-import { Fn } from "hotscript";
 import { ComponentType, FunctionComponent } from "react";
+import { OmitFn } from "./Fn";
 import { Hoc } from "./Hoc";
 import { newHoc } from "./newHoc";
 
-// https://github.com/gvergnaud/hotscript/issues/103
-interface OmitFn<Names extends string> extends Fn {
-  return: Omit<this["arg0"], Names>;
-}
-
 type WithOmitHoc = <OmitNames extends string>(
   omitNames: OmitNames[]
-) => Hoc<OmitFn<OmitNames>>;
+) => Hoc<[OmitFn<OmitNames>]>;
 
 export const withOmit = newHoc(function withOmit(
   Component: ComponentType,
--- src/withPick.tsx
+++ src/withPick.tsx
@@ -1,15 +1,11 @@
-import { Fn } from "hotscript";
 import { ComponentType, FunctionComponent } from "react";
+import { PickFn } from "./Fn";
 import { Hoc } from "./Hoc";
 import { newHoc } from "./newHoc";
 
-interface PickFn<Names extends string> extends Fn {
-  return: Pick<this["arg0"], Extract<Names, keyof this["arg0"]>>;
-}
-
 type WithPickHoc = <PickNames extends string>(
   pickNames: PickNames[]
-) => Hoc<PickFn<PickNames>>;
+) => Hoc<[PickFn<PickNames>]>;
 
 export const withPick = newHoc(function withPick(
   Component: ComponentType,
