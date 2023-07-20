a8dfb956ffd1c08f6f1afa6f03b62cae66b27eb8
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-07-20 11:07:24 -0300

    stress-test withWrapper

---
+++ test-d/stress-test/withWrapper.test-d.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent, ReactNode } from "react";
+import { expectType } from "tsd";
+import { stress } from "../../src/stress";
+import { withHocs } from "../../src/withHocs";
+import { withWrapper } from "../../src/withWrapper";
+
+const BeforeHoc: React.FC<{
+  a: string;
+  b: number;
+  c: boolean;
+}> = undefined as any;
+const Provider: React.FC<{ children: ReactNode }> = undefined as any;
+const AfterHoc = withHocs(stress(withWrapper(Provider)))(BeforeHoc);
+expectType<FunctionComponent<{ a: string; b: number; c: boolean }>>(AfterHoc);
