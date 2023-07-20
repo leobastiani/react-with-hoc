8688ae4008dc401c2795e317aaa6f2cbfcfd910f
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-07-19 23:35:53 -0300

    add withOmit

---
+++ test-d/stress-test/withOmit.test-d.tsx
@@ -0,0 +1,24 @@
+import { FunctionComponent } from "react";
+import { expectType } from "tsd";
+import { stress } from "../../src/stress";
+import { withBody } from "../../src/withBody";
+import { withHocs } from "../../src/withHocs";
+import { withOmit } from "../../src/withOmit";
+
+const BeforeHoc: React.FC<{
+  a: string;
+  b: number;
+  c: boolean;
+}> = undefined as any;
+const AfterHoc = withHocs(
+  stress(
+    withBody(({ b }: { b: number }) => ({ b })),
+    withOmit(["b"])
+  )
+)(BeforeHoc);
+expectType<
+  FunctionComponent<{
+    a: string;
+    c: boolean;
+  }>
+>(AfterHoc);
