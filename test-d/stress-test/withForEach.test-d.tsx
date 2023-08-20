9ad6a2faa30f7dd64722e0c4b7ddb4094c7f57fb
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-08-20 11:27:30 -0300

    test: create withForEach.test-d.tsx

---
+++ test-d/stress-test/withForEach.test-d.tsx
@@ -0,0 +1,19 @@
+import { FunctionComponent } from "react";
+import { expectType } from "tsd";
+import { stress } from "../../src/stress";
+import { withForEach } from "../../src/withForEach";
+import { withHocs } from "../../src/withHocs";
+
+const BeforeHoc: React.FC<{
+  a: string;
+  b: number;
+  c: boolean;
+}> = undefined as any;
+const AfterHoc = withHocs(stress(withForEach(10)))(BeforeHoc);
+expectType<
+  FunctionComponent<{
+    a: string;
+    b: number;
+    c: boolean;
+  }>
+>(AfterHoc);
