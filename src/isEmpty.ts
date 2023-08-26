75f3c6009f9fd7badea31d0fda1712a6e76e9071
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-08-26 10:06:39 -0300

    ts-prune

--- src/isEmpty.ts
+++
@@ -1,9 +0,0 @@
-export function isEmpty(obj: object | Array<any>): boolean {
-  if (Array.isArray(obj)) {
-    return !obj.length;
-  }
-  for (const _ in obj) {
-    return false;
-  }
-  return true;
-}
