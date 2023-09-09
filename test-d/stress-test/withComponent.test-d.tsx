35bec5865bebffe527f55974ad5af620fea32bca
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-09-09 14:35:01 -0300

    update test-d/stress-test/withComponents.test-d.tsx

--- test-d/stress-test/withComponent.test-d.tsx
+++ test-d/stress-test/withComponent.test-d.tsx
@@ -2,10 +2,10 @@
 import React, { FunctionComponent } from "react";
 import { expectType } from "tsd";
 import { PartialComponent } from "../../src/@types/PartialComponent";
-import { stress } from "../../src/stress";
+import { WithComponent } from "../../src/@types/WithComponent";
+import { halfStress, stress } from "../../src/stress";
 import { withComponent } from "../../src/withComponent";
 import { withHocs } from "../../src/withHocs";
-import { WithComponent } from "../../src/@types/WithComponent";
 
 function Button(props: {
   size: "lg" | "md" | "xs";
@@ -14,19 +14,40 @@ function Button(props: {
   return <button>{props.size}</button>;
 }
 
-const BeforeHoc: React.FC<{
-  Button: PartialComponent<typeof Button>;
-  oldProp: string;
-  oldPropOptional?: symbol;
-}> = undefined as any;
-const AfterHoc = withHocs(stress(withComponent("Button", Button)))(BeforeHoc);
-expectType<
-  FunctionComponent<{
+/* single */ {
+  const BeforeHoc: React.FC<{
+    Button: PartialComponent<typeof Button>;
     oldProp: string;
-    Button?: WithComponent<{
-      size: "lg" | "md" | "xs";
-      disabled?: boolean | undefined;
-    }>;
-    oldPropOptional?: symbol | undefined;
-  }>
->(AfterHoc);
+    oldPropOptional?: symbol;
+  }> = undefined as any;
+  const AfterHoc = withHocs(stress(withComponent("Button", Button)))(BeforeHoc);
+  expectType<
+    FunctionComponent<{
+      oldProp: string;
+      Button?: WithComponent<{
+        size: "lg" | "md" | "xs";
+        disabled?: boolean | undefined;
+      }>;
+      oldPropOptional?: symbol | undefined;
+    }>
+  >(AfterHoc);
+}
+
+/* multiple */ {
+  const BeforeHoc: React.FC<{
+    Button: PartialComponent<typeof Button>;
+    oldProp: string;
+    oldPropOptional?: symbol;
+  }> = undefined as any;
+  const AfterHoc = withHocs(halfStress(withComponent({ Button })))(BeforeHoc);
+  expectType<
+    FunctionComponent<{
+      oldProp: string;
+      Button?: WithComponent<{
+        size: "lg" | "md" | "xs";
+        disabled?: boolean | undefined;
+      }>;
+      oldPropOptional?: symbol | undefined;
+    }>
+  >(AfterHoc);
+}
