c417b93783f87947766fbeafbda2f8695f9f5196
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2024-01-08 17:55:29 -0300

    update withHocs docs

--- src/hocs/withHocs.tsx
+++ src/hocs/withHocs.tsx
@@ -23,16 +23,16 @@ type WithHocsFlat<
  * ```
  *
  * @example
- * import { withHoc1, withHoc1 } from 'my-hocs'
- * // `withHoc1_2` can be used somewhere else
- * export const withHoc1_2 = withHocs([withHoc1, withHoc2])
- * @example
  * const MyComponent = (() => {
  *   function MyComponent(props: { ... }) {
  *     return <OtherComponents />;
  *   }
  *   return withHocs([ ... ])(MyComponent);
  * })();
+ * @example
+ * import { withHoc1, withHoc1 } from 'my-hocs'
+ * // `withHoc1_2` can be used somewhere else
+ * export const withHoc1_2 = withHocs([withHoc1, withHoc2])
  */
 export function withHocs<const Hocs extends readonly WithHocsArg[]>(
   hocs: Hocs,
