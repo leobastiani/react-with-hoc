c46b71114da3b2d2f2497f40f9886f37b7cfcdb4
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2024-01-14 16:24:43 -0300

    add withForEach docs

--- src/hocs/withForEach.test.tsx
+++ src/hocs/withForEach.test.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { render } from "@testing-library/react";
+import React from "react";
 import { componentDisplayName } from "../utils/componentDisplayName";
 import { withForEach } from "./withForEach";
 
@@ -10,7 +10,18 @@ interface ExampleProps {
 }
 
 function Example(props: ExampleProps): JSX.Element {
-  return <pre>{JSON.stringify(props)}</pre>;
+  // @ts-expect-error it's okay
+  // eslint-disable-next-line react/prop-types
+  const { children, ...rest } = props;
+  return (
+    <pre>
+      {"\n"}
+      {JSON.stringify(rest)}
+      {"\n"}
+      {children ? <>children: {children}</> : ""}
+      {!!children && "\n"}
+    </pre>
+  );
 }
 
 it("withForEach name", () => {
@@ -21,31 +32,65 @@ it("withForEach name", () => {
 it("withForEach number", () => {
   const Component = withForEach(2)(Example);
   render(<Component a={1} b={2} />);
-  expect(document.body.children[0].innerHTML).toBe(
-    '<pre>{"a":1,"b":2,"i":0}</pre><pre>{"a":1,"b":2,"i":1}</pre>',
-  );
+  expect(document.body.children[0].innerHTML).toMatchInlineSnapshot(`
+"<pre>
+{"a":1,"b":2,"i":0}
+</pre><pre>
+{"a":1,"b":2,"i":1}
+</pre>"
+`);
 });
 
 it("withForEach array of string", () => {
   const Component = withForEach(["c", "d"])(Example);
   render(<Component a={1} b={2} />);
-  expect(document.body.children[0].innerHTML).toBe(
-    '<pre>{"a":1,"b":2,"i":0,"children":"c"}</pre><pre>{"a":1,"b":2,"i":1,"children":"d"}</pre>',
-  );
+  expect(document.body.children[0].innerHTML).toMatchInlineSnapshot(`
+"<pre>
+{"a":1,"b":2,"i":0}
+children: c
+</pre><pre>
+{"a":1,"b":2,"i":1}
+children: d
+</pre>"
+`);
 });
 
 it("withForEach array of objects", () => {
   const Component = withForEach([{ c: 3 }, { d: 4 }])(Example);
   render(<Component a={1} b={2} />);
-  expect(document.body.children[0].innerHTML).toBe(
-    '<pre>{"children":{"c":3},"c":3,"a":1,"b":2,"i":0}</pre><pre>{"children":{"d":4},"d":4,"a":1,"b":2,"i":1}</pre>',
-  );
+  expect(document.body.children[0].innerHTML).toMatchInlineSnapshot(`
+"<pre>
+{"c":3,"a":1,"b":2,"i":0}
+</pre><pre>
+{"d":4,"a":1,"b":2,"i":1}
+</pre>"
+`);
+});
+
+it("withForEach array of elements", () => {
+  const Component = withForEach([
+    <div key="div-1">1</div>,
+    <div key="div-2">2</div>,
+  ])(Example);
+  render(<Component a={1} b={2} />);
+  expect(document.body.children[0].innerHTML).toMatchInlineSnapshot(`
+"<pre>
+{"a":1,"b":2,"i":0}
+children: <div>1</div>
+</pre><pre>
+{"a":1,"b":2,"i":1}
+children: <div>2</div>
+</pre>"
+`);
 });
 
 it("withForEach object", () => {
   const Component = withForEach({ c: 3 })(Example);
   render(<Component a={1} b={2} />);
-  expect(document.body.children[0].innerHTML).toBe(
-    '<pre>{"children":3,"a":1,"b":2,"i":"c"}</pre>',
-  );
+  expect(document.body.children[0].innerHTML).toMatchInlineSnapshot(`
+"<pre>
+{"a":1,"b":2,"i":"c"}
+children: 3
+</pre>"
+`);
 });
--- src/hocs/withForEach.tsx
+++ src/hocs/withForEach.tsx
@@ -1,12 +1,65 @@
-import React from "react";
-import { ComponentType, FunctionComponent, Key } from "react";
+import React, {
+  ComponentType,
+  FunctionComponent,
+  Key,
+  ReactNode,
+  isValidElement,
+} from "react";
 import { SetOptionalFn } from "../types/Fn";
 import { Hoc } from "../types/Hoc";
 import { newHoc } from "../utils/newHoc";
 
 interface Options<IndexName extends string, ValueName extends string> {
+  /**
+   * @default "i"
+   *
+   * @example
+   * //                                            ↓ "someName" here as well
+   * const Component = withForEach(3, {indexName: "someName"})(Example)
+   * <Component {...props} />
+   * // is equivalent to
+   * <>
+   *   //                          ↓ "someName" here as well
+   *   <Example key={0} {...props} someName={0} />
+   *   <Example key={1} {...props} someName={1} />
+   *   <Example key={2} {...props} someName={2} />
+   * </>
+   */
   indexName?: IndexName;
+  /**
+   * @default "children"
+   *
+   * @example
+   * const Component = withForEach(
+   *   ["a", "b"],
+   *   //           ↓ "someName" here as well
+   *   {valueName: "someName"}
+   * )(Example)
+   * <Component {...props} />
+   * // is equivalent to
+   * <>
+   *   //                                ↓ "someName" here as well
+   *   <Example key={0} {...props} i={0} someName="a" />
+   *   <Example key={1} {...props} i={1} someName="b" />
+   * </>
+   */
   valueName?: ValueName;
+  /**
+   * @default (props) => props[indexName] // which is props.i
+   *
+   * @example
+   * // makes key = 2^x
+   * const Component = withForEach(4, {key: ({i}) => 2 ** i})(Example)
+   * <Component {...props} />
+   * // is equivalent to
+   * <>
+   *   //            ↓ note key values
+   *   <Example key={1} {...props} someName={0} />
+   *   <Example key={2} {...props} someName={1} />
+   *   <Example key={4} {...props} someName={2} />
+   *   <Example key={8} {...props} someName={2} />
+   * </>
+   */
   key?: (props: object) => Key;
 }
 
@@ -24,53 +77,114 @@ interface WithForEachHoc {
   >;
 }
 
+const isReactNode = (value: unknown): boolean =>
+  isValidElement(value) || typeof value !== "object" || value === null;
+
+/**
+ * Like Array.map but for React components.
+ * Concatenates components for each item in array, range or object
+ *
+ * @experimental It needs to discuss its name
+ *
+ * @example
+ * const Component = withForEach(3)(Example)
+ * <Component {...props} />
+ * // is equivalent to
+ * <>
+ *   <Example key={0} {...props} i={0} />
+ *   <Example key={1} {...props} i={1} />
+ *   <Example key={2} {...props} i={2} />
+ * </>
+ *
+ * @example
+ * const Component = withForEach(["a", "b"])(Example)
+ * <Component {...props} />
+ * // is equivalent to
+ * <>
+ *   <Example key={0} {...props} i={0}>a</Example>
+ *   <Example key={1} {...props} i={1}>b</Example>
+ * </>
+ *
+ * @example
+ * const Component = withForEach({
+ *   a: 100,
+ *   b: 200,
+ * })(Example)
+ * <Component {...props} />
+ * // is equivalent to
+ * <>
+ *   <Example key="a" {...props} i="a">100</Example>
+ *   <Example key="b" {...props} i="b">200</Example>
+ * </>
+ */
 export const withForEach = newHoc<WithForEachHoc>(function withForEach(
   Component: ComponentType,
-  target: number | Array<unknown> | object | string,
+  target: number | Array<unknown> | object,
   {
     indexName = "i",
     key = (props: any): Key => props[indexName],
     valueName = "children",
   } = {},
 ): FunctionComponent {
-  return function WithForEach(props: any): JSX.Element {
-    let ret: any;
+  function componentWithKey(newProps: any): ReactNode {
+    if (valueName === "children") {
+      const { children, ...rest } = newProps;
+      return (
+        <Component key={key(newProps)} {...rest}>
+          {children}
+        </Component>
+      );
+    }
+    return <Component key={key(newProps)} {...newProps} />;
+  }
+
+  return function WithForEach(props: any): ReactNode {
     if (typeof target === "number") {
-      ret = Array.from({ length: target }).map((_v, i) => (
-        <Component
-          key={key({ ...props, [indexName]: i })}
-          {...{ ...props, [indexName]: i }}
-        />
-      ));
-    } else if (Array.isArray(target)) {
-      if (target.length > 0 && typeof target[0] != "object") {
-        ret = target.map((v, i) => (
-          <Component
-            key={key({ ...props, [indexName]: i, [valueName]: v })}
-            {...{ ...props, [indexName]: i, [valueName]: v }}
-          />
-        ));
-      } else {
-        ret = target.map((v, i) => (
-          <Component
-            key={key({ ...props, [indexName]: i })}
-            {...{ [valueName]: v, ...v, ...props, [indexName]: i }}
-          />
-        ));
-      }
-    } else if (typeof target === "object" && target !== null) {
-      ret = Object.entries(target).map(([index, value]) => (
-        <Component
-          key={key({ ...props, [indexName]: index })}
-          {...{ [valueName]: value, ...props, [indexName]: index }}
-        />
-      ));
-    } else if (process.env.NODE_ENV !== "production") {
-      throw new Error(
-        "withForEach: should be used with number, Array or object",
+      return Array.from({ length: target }).map((_v, i) =>
+        componentWithKey({ ...props, [indexName]: i }),
       );
     }
 
-    return ret;
+    if (Array.isArray(target)) {
+      if (target.length === 0) {
+        return target as ReactNode[];
+      }
+
+      if (isReactNode(target[0])) {
+        return target.map((v, i) =>
+          componentWithKey({ ...props, [indexName]: i, [valueName]: v }),
+        );
+      }
+
+      return target.map((v, i) =>
+        componentWithKey({ ...v, ...props, [indexName]: i }),
+      );
+    }
+
+    if (typeof target === "object" && target !== null) {
+      if (Object.keys(target).length === 0) {
+        return null;
+      }
+
+      if (isReactNode(Object.values(target)[0])) {
+        return Object.entries(target).map(([index, value]) => {
+          return componentWithKey({
+            ...props,
+            [indexName]: index,
+            [valueName]: value,
+          });
+        });
+      }
+
+      return Object.entries(target).map(([index, value]) => {
+        return componentWithKey({ ...value, ...props, [indexName]: index });
+      });
+    }
+
+    if (process.env.NODE_ENV !== "production") {
+      const never: never = target;
+      never;
+    }
+    throw new Error("withForEach: should be used with number, Array or object");
   };
 });
