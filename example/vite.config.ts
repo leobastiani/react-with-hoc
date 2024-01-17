86ca7617faba73922772656cd7e31c0dc5e60b9b
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2024-01-16 22:34:37 -0300

    update example

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "",
+  build: {
+    outDir: "../docs/example",
+    sourcemap: true,
+    emptyOutDir: true,
+  },
   plugins: [react()],
 });
